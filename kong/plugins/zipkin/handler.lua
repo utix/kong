449d3c15e2aabd61ccfecd1573aa33805d06146d
Mayo <i@shoujo.io>
2022-05-24 16:07:37 +0800

    fix(zipkin) include connect time in balancer span

--- kong/plugins/zipkin/handler.lua
+++ kong/plugins/zipkin/handler.lua
@@ -3,8 +3,12 @@ local new_span = require "kong.plugins.zipkin.span".new
 local utils = require "kong.tools.utils"
 local tracing_headers = require "kong.plugins.zipkin.tracing_headers"
 local request_tags = require "kong.plugins.zipkin.request_tags"
+local ngx_re = require "ngx.re"
 
 
+local ngx = ngx
+local ngx_var = ngx.var
+local split = ngx_re.split
 local subsystem = ngx.config.subsystem
 local fmt = string.format
 local rand_bytes = utils.get_rand_bytes
@@ -327,6 +331,7 @@ function ZipkinLogHandler:log(conf) -- luacheck: ignore 212
   local balancer_data = ngx_ctx.balancer_data
   if balancer_data then
     local balancer_tries = balancer_data.tries
+    local upstream_connect_time = split(ngx_var.upstream_connect_time, ",", "jo")
     for i = 1, balancer_data.try_count do
       local try = balancer_tries[i]
       local name = fmt("%s (balancer try %d)", request_span.name, i)
@@ -344,7 +349,8 @@ function ZipkinLogHandler:log(conf) -- luacheck: ignore 212
       tag_with_service_and_route(span)
 
       if try.balancer_latency ~= nil then
-        span:finish((try.balancer_start + try.balancer_latency) * 1000)
+        local try_connect_time = (upstream_connect_time[i] or 0) * 1000 -- ms
+        span:finish((try.balancer_start + try.balancer_latency + try_connect_time) * 1000)
       else
         span:finish(now_mu)
       end
